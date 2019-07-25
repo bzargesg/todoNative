a1a5782da1a0b2e8426bb8db5ffbe1ebabac8597
bzargesg <bzargesg@gmail.com>
2019-07-25 13:59:34 -0600

    basic todoList added

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableHighlight } from 'react-native';
 
-export default function App() {
-  return (
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      todos: [],
+      newTodo: ''
+    }
+    this.text = '';
+  }
+  textChange(text){
+    this.setState({newTodo:text})
+  }
+  buttonClick(e){
+    let newValue = [...this.state.todos, this.state.newTodo]
+    this.setState({
+      todos: newValue
+    })
+  }
+  render(){
+    return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Todo List length{this.state.todos.length}</Text>
+      <TextInput value={this.state.newTodo} onChangeText={this.textChange.bind(this)}/>
+      <TouchableHighlight onPress={this.buttonClick.bind(this)}>
+        <Text>tap me</Text>
+      </TouchableHighlight>
+      <View>
+      {this.state.todos.map((todo, i)=><Text key={i}>{todo}</Text>)}
+      </View>
     </View>
   );
+  }
 }
 
 const styles = StyleSheet.create({
