23334b9d6057b6ced2db0865218084bae8f7c170
bzargesg <bzargesg@gmail.com>
2019-07-25 15:01:18 -0600

    more style play

--- components/FancyComponent.js
+++ components/FancyComponent.js
@@ -2,13 +2,19 @@ import React from 'react';
 import {
   StyleSheet,
   View,
-  Text
+  Text,
+  Dimensions
 } from 'react-native';
+const {width, height} = Dimensions.get('window');
+
 export class Fancy extends React.Component{
   render(){
     return(
       <View style={styles.container}>
-        <Text>Fancy!</Text>
+        <View style={styles.box}>
+
+        </View>
+        <Text style={styles.text}>Fancy!</Text>
       </View>
     )
   }
@@ -16,6 +22,24 @@ export class Fancy extends React.Component{
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red'
+    backgroundColor: 'red',
+    padding: 40,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  box: {
+    backgroundColor: 'yellow',
+    width: width/2,
+    height: height / 3,
+    position: 'absolute',
+    top: 20,
+    left: 30,
+    borderRadius: 15,
+  },
+  text: {
+    color: 'white',
+    fontSize: 34,
+    fontFamily: 'arial',
+    fontWeight: '600'
   }
 })
