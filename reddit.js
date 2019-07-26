a14bdd82bcaa62dbb8e49b4aa2389671ac6f61dd
bzargesg <bzargesg@gmail.com>
2019-07-26 08:51:03 -0600

    basic fetch and display

--- reddit.js
+++ reddit.js
@@ -2,17 +2,32 @@ import React from 'react';
 import {View, Text} from 'react-native';
 
 export class Reddit extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      posts: []
+    }
+  }
   componentWillMount(){
-    fetch('https://www.reddit.com/json',
+    fetch('https://www.reddit.com/.json',
     {Accept: 'application/json'}
     ).then(res => res.json())
-    .then(data=>console.log(data))
+    .then(data=>{
+      this.setState({
+        posts: data.data.children
+      })
+    })
   }
   render(){
     
     return(
       <View>
         <Text>reddit</Text>
+        <View>
+          {this.state.posts.map((post, i)=>{
+            return(<Text key={i}>{post.data.author}</Text>)
+          })}
+        </View>
       </View>
     );
   }
